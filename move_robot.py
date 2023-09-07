2dda683cb9f4e58462951e859dd6d75199aa1728
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2023-09-07 10:30:11 -0500

    Update move_robot.py

--- move_robot.py
+++ move_robot.py
@@ -1,17 +1,21 @@
 """
 Drive your robot to make it:
-  1. Go forward 2 seconds
-  2. Spin left 2 seconds
-  3. Go backward 2 seconds
-  4. Spin right 2 seconds
+  1. move forward 2 seconds
+  2. move backward 2 seconds
+  3. move forward and left 2 seconds
+  4. move backward and right 2 seconds
+  5. move forward and right 2 seconds
+  6. move backward and left 2 seconds
+  7. spin left in place 2 seconds
+  8. spin right in place 2 seconds
+  9. stop
   
 """
+from time import sleep
+from machine import Pin, PWM, Timer
 
-import time
-from gpiozero import Robot
+
+# SETUP
 
 
-#### Write your code below ####
-# robot = Robot()
-
-# While Ture:
+# LOOP
